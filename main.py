8d4ee3286c8520e1b8786df0b354fe175e94ca60
takanami <mixjobs1228@yahoo.co.jp>
2022-05-10 09:26:26 +0900

    yamlファイルの書き、読みを追加 yamlファイルを作成

--- main.py
+++ main.py
@@ -1,41 +1,25 @@
 """
-今回Configファイルに書き込む設定
-[DEFAULT]
-debug = True
+yamlファイルに書き込む設定
+web_server:
+    host:127.0.0.1
+    port:80
 
-[web_server]
-host = 127.0.0.1
-port = 80
-
-[db_server]
-host = 127.0.0.1
-port = 3306
+db_server:
+    host:127.0.0.1
+    port:3306
 """
+import yaml
 
-import configparser
-
-# Configファイルに書き込む
-# config = configparser.ConfigParser()
-# config['DEFAULT'] = {
-#     'debug': True
-# }
-#
-# config['web_server'] = {
-#     'host': '127.0.0.1',
-#     'port': 80
-# }
-#
-# config['db_server'] = {
-#     'host': '127.0.0.1',
-#     'port': 3306
-# }
-#
-# with open('config.ini', 'w') as config_file:
-#     config.write(config_file)
-
-# Configファイルから読みだす
-config = configparser.ConfigParser()
-config.read('config.ini')
-print(config['web_server'])
-print(config['web_server'], ['host'])
-print(config['web_server'], ['port'])
+# yamlファイルの作成
+with open('config.yaml', 'w') as yaml_file:
+    yaml.dump({
+        'web_server': {
+            'host': '127.0.0.1',
+            'port': 80
+        },
+        'db_server': {
+            'host': '127.0.0.1',
+            'port': 3306
+        }
+        # yamlファイルないの記述をブロックスタイルにするにはdefault_flow_style=Falseに
+    }, yaml_file, default_flow_style=False)
