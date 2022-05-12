4facb2248816ca25c0fde11aacc1f89e65d92af1
takanami <mixjobs1228@yahoo.co.jp>
2022-05-12 09:28:36 +0900

    loggingの使い方4 ハンドラーを使ってログをファイル出力

--- logtest.py
+++ logtest.py
@@ -1,8 +1,19 @@
 import logging
 
+if __name__ == '__main__':
+    print("mainとして実行します")
+else:
+    print("importされて実行します")
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+# logtest.logのハンドルをloggerに渡す
+# loggerがだすlogのみがlogtest.logに記述される
+h = logging.FileHandler('logtest.log')
+logger.addHandler(h)
+
 
 def do_log_test():
-    logger = logging.getLogger(__name__)
-    logger.setLevel(logging.DEBUG)
     logger.info('test info')
     logger.debug('debug test')
